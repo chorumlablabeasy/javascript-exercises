574909c59a274c6a9abc69683e6c9df2996c75da
mami <115097751+chorumlablabeasy@users.noreply.github.com>
2025-04-28 00:26:52 +0300

    finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    const fibonacci = [0, 1, 1]
+    if (typeof(number) === "string") { Number(number) }
+    if (number < 0) { return "OOPS" }
+    for (let i = 3; i <= number; i++) {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
+    }
+    return fibonacci[number]
 };
 
 // Do not edit below this line
