1ddbfa287e8210f10c84690558a8d20cb1f0ca08
mami <115097751+chorumlablabeasy@users.noreply.github.com>
2025-04-27 23:55:36 +0300

    finish exercises 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const strArr = str.split("").map(element => element.toLowerCase())
+    const filteredStrArr = strArr.filter(element => {
+        return element.match(/[a-zA-Z0-9]/)
+    })
+    return filteredStrArr.length === filteredStrArr.reverse().length && filteredStrArr.every((value, index) => value === filteredStrArr.reverse()[index])
 };
 
 // Do not edit below this line
