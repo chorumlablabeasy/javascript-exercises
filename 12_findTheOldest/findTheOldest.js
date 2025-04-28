333c0ff0c06fb6e479470c09fa5227a1a2a9e633
mami <115097751+chorumlablabeasy@users.noreply.github.com>
2025-04-28 17:24:14 +0300

    finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    const thisYear = new Date().getFullYear();
+    const avrLiveSpan = 80
+    const editedArr = people.map((person) => {
+        if (person.yearOfDeath === undefined && thisYear - person.yearOfBirth > avrLiveSpan) {
+            person.yearOfDeath = person.yearOfBirth + avrLiveSpan} 
+        else if (person.yearOfDeath === undefined && thisYear - person.yearOfBirth < avrLiveSpan) {
+            person.yearOfDeath = thisYear
+            }
+    return person
+})
+    editedArr.sort((a,b) => {
+        const ageA = a.yearOfDeath - a.yearOfBirth
+        const ageB = b.yearOfDeath - b.yearOfBirth
+        return ageB - ageA
+    })
+    return editedArr[0]
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
